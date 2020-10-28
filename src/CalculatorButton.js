6610d9838ba68906e5b609838b7d2e9a2d47aba7
Maksim <sargaras_m@mail.ru>
2020-10-29 00:47:13 +0300

    First

---
+++ src/CalculatorButton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function CalculatorButton(props){
+
+    return (
+        <input type='button'
+                value={props.caption}/>
+    )
+
+}
+
+export default CalculatorButton
